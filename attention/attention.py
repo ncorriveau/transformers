2013d814028193a3309befb26da8fae491a5a720
Nicko Corriveau <nc2942@columbia.edu>
2024-07-28 12:34:37 -0400

    clean up and initial mask class

--- attention/attention.py
+++ attention/attention.py
@@ -1,5 +1,6 @@
 ## implement popular attention variants using a generalized framework in PyTorch ##
 import math
+from functools import cached_property
 
 import torch
 import torch.nn as nn
@@ -17,8 +18,50 @@ Design:
         could also be viewed as a form of mask. 
         Look into global + sliding window attention as well to see if 
         this fits the mask paradigm. 
+    Engines: we will separate out the actual computatational engine for the 
+    attention operations into separate modules that can be used with 
+    your specific attention variation. Starter ideasa include:
+        - FlashAttention
+        - Paged Attention 
+        - Ring Attention
 
 """
+
+
+class Mask:
+    """
+    A class to implement many of the attention variants that can be attributed
+    to different masking techniques on the attention matrix to reduce computation.
+    A good overview of a lot of the methods can be found here:
+        https://arxiv.org/abs/2004.05150v2?ref=research.character.ai
+
+    """
+
+    def __init__(self, context_size: int):
+        self.context_size = context_size
+
+    @cached_property
+    def causal_mask(self):
+        return torch.tril(
+            torch.ones([self.context_size, self.context_size], dtype=torch.bool),
+            diagonal=1,
+        )
+
+    @cached_property
+    def sliding_window_mask(self, window_size: int):
+        pass
+
+    @cached_property
+    def global_mask(self):
+        pass
+
+    @cached_property
+    def dilated_sliding_mask(self):
+        pass
+
+    @cached_property
+    def streaming_mask(self):
+        pass
 
 
 class Attention(nn.Module):
@@ -57,15 +100,18 @@ class Attention(nn.Module):
         self.num_heads_k = num_heads_k
         self.num_heads_v = num_heads_v
 
-        self.dim_q = hidden_size // num_heads_q
+        self.dim_q_k = hidden_size // num_heads_q  # shared between Q and K matrices
         self.dim_v = hidden_size // num_heads_v
 
         self.context_size = context_size
         self.attn_drop = nn.Dropout(attn_drop)
         self.output_drop = nn.Dropout(output_drop)
 
-        self.WQ = nn.Linear(hidden_size, self.dim_q * num_heads_q)
-        self.WK = nn.Linear(hidden_size, self.dim_q * num_heads_k)
+        # WQ will always be hidden_size -> hidden_size but is shown
+        # as hidden_size -> dim_q_k * num_heads_q to emphasize the general pattern
+        self.WQ = nn.Linear(hidden_size, self.dim_q_k * num_heads_q)
+
+        self.WK = nn.Linear(hidden_size, self.dim_q_k * num_heads_k)
         self.WV = nn.Linear(hidden_size, self.dim_v * num_heads_v)
         self.W_0 = nn.Linear(num_heads_q * self.dim_v, hidden_size)
 
@@ -77,9 +123,9 @@ class Attention(nn.Module):
         B, S, D = input.size()  # batch size, sequence length, hidden dim
 
         Q: torch.Tensor = self.WQ(input).reshape(
-            B, num_heads_q // num_heads_k, num_heads_k, S, self.dim_q
+            B, num_heads_q // num_heads_k, num_heads_k, S, self.dim_q_k
         )
-        K: torch.Tensor = self.WK(input).reshape(B, num_heads_k, S, self.dim_q)
+        K: torch.Tensor = self.WK(input).reshape(B, num_heads_k, S, self.dim_q_k)
         V: torch.Tensor = self.WV(input).reshape(B, num_heads_v, S, self.dim_v)
 
         attn: torch.Tensor = (Q @ K.transpose(-2, -1)).reshape(B, num_heads_q, S, S)
