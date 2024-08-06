fc9481a155d21360c7d3e56ec8d924249ed02d55
Nicko Corriveau <nc2942@columbia.edu>
2024-08-06 19:57:01 -0400

    add dataloader to training loop

--- src/train.py
+++ src/train.py
@@ -21,33 +21,38 @@ class OptimizerConfig:
 
 # TODO fill out to read in from config yaml
 def load_optimizer_params(optimizer_config: str):
-    return OptimizerConfig(lr=1e-3, betas=(0.9, 0.999))
+    return OptimizerConfig(lr=3e-4, betas=(0.9, 0.999))
 
 
-def create_data_set(
-    tokenizer: str,
-    batch_size: int = 32,
-    seq_len: int = 128,
-    data_path: str = "./data/tiny_shakespeare.txt",
-):
-    """
-    taken from karpathy's preferred way of loading / training on small
-    data set
-    """
-    with open(data_path, "r") as f:
-        data = f.read()
+class TokenDataSet(torch.utils.data.Dataset):
+    def __init__(self, tokenizer: str, seq_len: int, data_path: str):
+        try:
+            with open(data_path, "r") as f:
+                data = f.read()
 
-    enc = tiktoken.get_encoding(tokenizer)
-    tokens = enc.encode(data)
-    buf = torch.tensor(tokens[: batch_size * seq_len + 1])
+        except Exception as e:
+            print(f"Error loading data: {e}")
+            raise e
 
-    x = buf[:-1].view(batch_size, seq_len)
-    y = buf[1:].view(batch_size, seq_len)
-    return x, y
+        enc = tiktoken.get_encoding(tokenizer)
+        self.tokens = enc.encode(data)
+        self.seq_len = seq_len
+
+    def __len__(self):
+        return len(self.tokens) - self.seq_len
+
+    def __getitem__(self, idx):
+        x = self.tokens[idx : idx + self.seq_len]
+        y = self.tokens[idx + 1 : idx + self.seq_len + 1]
+        return torch.tensor(x), torch.tensor(y)
+
+    def __repr__(self):
+        return f"TokenDataSet with {len(self.tokens)} tokens"
 
 
 def train(model: str, optimizer_config: str, epochs: int):
-    # load model
+
+    # load model config - this should include seq
     model_config: ModelConfig = load_model_config(model)
 
     # load optimizer parameters
@@ -61,21 +66,22 @@ def train(model: str, optimizer_config: str, epochs: int):
         model.parameters(), lr=optimizer_params.lr, betas=optimizer_params.betas
     )
 
-    X, Y = create_data_set("gpt2")
-    dataset = torch.utils.data.TensorDataset(X, Y)
+    # TODO: we need seq_len, batch_size from model config
+    dataset = TokenDataSet("gpt2", 128, "./data/tiny_shakespeare.txt")
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
 
     # training loop
-    for _ in range(epochs):
-        optimizer.zero_grad()
-
-        # size, B, S, Vocab Size
-        output = model(X)
-
-        loss = F.cross_entropy(output.view(-1, output.size(-1)), Y.view(-1))
-        print(loss)
-        loss.backward()
-        # optimizer.step()
-        # print(f"Epoch: {epoch}, Loss: {loss.item()}")
+    for epoch in range(epochs):
+        step = 0
+        for x, y in data_loader:
+            optimizer.zero_grad()
+            # shape B, S, V
+            output = model(x)
+            loss = F.cross_entropy(output.view(-1, output.size(-1)), y.view(-1))
+            loss.backward()
+            optimizer.step()
+            print(f"step: {step}, Loss: {loss.item()}")
+            step += 1
 
     return model
 
