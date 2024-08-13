5753e664c0db639ab7f0bf95bf3d0bd24acd4cbe
Nicko Corriveau <nc2942@columbia.edu>
2024-08-13 11:22:18 -0400

    add dist training set up

--- src/train.py
+++ src/train.py
@@ -2,15 +2,25 @@
 Training loop for a generic LLM model here
 """
 
+import os
 from dataclasses import dataclass
 
 import click
 import tiktoken
 import torch
+import torch.distributed as dist
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+from torch.distributed.fsdp.fully_sharded_data_parallel import (
+    BackwardPrefetch,
+    CPUOffload,
+)
+from torch.nn.parallel import DataParallel
+from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.optim import Optimizer
 from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
 
 from .model import CausalLLM, ModelConfig
 from .utils import TrainingConfig, build_model_config, build_training_config
@@ -42,6 +52,45 @@ class TokenDataSet(torch.utils.data.Dataset):
         return f"TokenDataSet with {len(self.tokens)} tokens"
 
 
+def setup(dataset, backend: str = "nccl") -> tuple[int, int]:
+    rank = int(os.environ.get("RANK", 0))
+    world_size = int(os.environ.get("WORLD_SIZE", 1))
+    # if world_size > 1 then we start a dist process
+    if world_size - 1 and rank:
+        dist.init_process_group(backend=backend, rank=rank, world_size=world_size)
+        device = torch.device(f"cuda:{rank}" if torch.cuda.is_available() else "cpu")
+        sampler = DistributedSampler(dataset, rank=rank, num_replicas=world_size)
+        shuffle = False
+    else:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        sampler = None
+        shuffle = True
+    return {
+        "rank": rank,
+        "world_size": world_size,
+        "device": device,
+        sampler: sampler,
+        shuffle: shuffle,
+    }
+
+
+def distribute_model(model: nn.Module, state: dict, strategy: str) -> nn.Module:
+    if state["world_size"] == 1:
+        return model
+    match strategy:
+        case "ddp":
+            model = DDP(model, device_ids=[state["rank"]])
+        case "fsdp":
+            model = FSDP(
+                model,
+                cpu_offload=CPUOffload(offload_params=True),
+                backward_prefetch=BackwardPrefetch.BACKWARD_PRE,
+            )
+        case "data_parallel":
+            model = DataParallel(model, device_ids=[state["rank"]])
+    return model
+
+
 @click.command()
 @click.option("--model-config-path", type=str, required=True)
 @click.option("--training-config-path", type=str, required=True)
@@ -50,17 +99,27 @@ def train(model_config_path: str, training_config_path: str, data_path: str):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model_config: ModelConfig = build_model_config(model_config_path)
     training_config: TrainingConfig = build_training_config(training_config_path)
+    dataset = TokenDataSet("gpt2", model_config.common.context_size, data_path)
+    # process state
+    state = setup()
 
     # instantiate model, optimizer and data loader
-    model = CausalLLM(model_config).to(device)
+    model = CausalLLM(model_config).to(state["device"])
     optimizer: Optimizer = training_config.partial_optimizer(model.parameters())
-    dataset = TokenDataSet("gpt2", model_config.common.context_size, data_path)
+
+    model = distribute_model(model, state, training_config.distributed_strategy)
+
     data_loader = DataLoader(
-        dataset, batch_size=training_config.batch_size, shuffle=True
+        dataset,
+        batch_size=training_config.batch_size,
+        sampler=state["sampler"],
+        shuffle=state["shuffle"],
     )
 
     # training loop
-    for _ in range(training_config.epochs):
+    for epoch in range(training_config.epochs):
+        if state["sampler"]:
+            state["sampler"].set_epoch(epoch)
         step = 0
         for x, y in data_loader:
             optimizer.zero_grad()
