7b47484ac3fca7f0a56028c1160034b81a1242db
Nicko Corriveau <nc2942@columbia.edu>
2024-09-05 19:53:56 -0700

    update w pytorch implementation

--- src/transformer/positional_encoding.py
+++ src/transformer/positional_encoding.py
@@ -1,6 +1,9 @@
+from typing import Optional
+
 import numpy as np
 import torch
 import torch.nn as nn
+from torchtune.modules.position_embeddings import RotaryPositionalEmbeddings
 
 
 class PositionalEncoding(nn.Module):
@@ -34,30 +37,74 @@ class SinusoidalPE(PositionalEncoding):
 
 
 class RoPE(PositionalEncoding):
-    def __init__(self, hidden_size: int, context_size: int, base: int = 10_000, m=1):
+    def __init__(self, hidden_size: int, context_size: int, base: int = 10_000):
         super().__init__(hidden_size, context_size)
-        self.thetas = (
-            np.power(base, -2 * np.arange(0, hidden_size // 2) / hidden_size) * m
+        self.thetas = torch.float_power(
+            base, -2 * torch.arange(0, hidden_size // 2) / hidden_size
         )
-        rope = torch.zeros(hidden_size, hidden_size)
-        for i in range(len(self.thetas)):
-            idx = i * 2
-            rope[idx, idx] = np.cos(self.thetas[i])
-            rope[idx, idx + 1] = -np.sin(self.thetas[i])
-            rope[idx + 1, idx] = np.sin(self.thetas[i])
-            rope[idx + 1, idx + 1] = np.cos(self.thetas[i])
+        self.build_rope_cache()
 
-        self.register_buffer("rope", rope, persistent=False)
+    def build_rope_cache(self, max_seq_len: int = 4096) -> None:
+        # Create position indexes `[0, 1, ..., max_seq_len - 1]`
+        # this is our i's
+        seq_idx = torch.arange(max_seq_len)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return x @ self.rope
+        # Outer product of theta and position index; output tensor has
+        # a shape of [max_seq_len, dim // 2]
+        # for ex here we have 2 thetas for hidden dim = 4
+        # so we produce a tensor of shape [max_seq_len, 2]
+        idx_theta = torch.outer(seq_idx, self.thetas).float()
+
+        # cache includes both the cos and sin components and so the output shape is
+        # [max_seq_len, dim // 2, 2]
+        # so here we just calculate cos, sin for all of our thetas thus the dim 2 = cos, sin and dim // 2 = thetas
+        cache = torch.stack([torch.cos(idx_theta), torch.sin(idx_theta)], dim=-1)
+        self.register_buffer("cache", cache, persistent=False)
+
+    def forward(self, x: torch.Tensor, input_pos: Optional[int] = None) -> torch.Tensor:
+        # input tensor has shape [b, s, n_h, h_d]
+        seq_len = x.size(1)
+
+        # extract the values based on whether input_pos is set or not
+        rope_cache = self.cache[input_pos] if input_pos else self.cache[:seq_len]
+
+        # reshape input; the last dimension is used for computing the output.
+        # Cast to float to match the reference implementation
+        # tensor has shape [b, s, n_h, h_d // 2, 2]
+        x_shaped = x.float().reshape(*x.shape[:-1], -1, 2)
+
+        # reshape the cache for broadcasting
+        # tensor has shape [b, s, 1, h_d // 2, 2] if packed samples,
+        # otherwise has shape [1, s, 1, h_d // 2, 2]
+        rope_cache = rope_cache.view(-1, x_shaped.size(1), 1, x_shaped.size(3), 2)
+
+        # so x is now chopped up with each feature split into 2 and we have hidden_dim // 2 of these
+        # we can then calculate their rotation by multiplying the first part with the cos
+        # and the second part with the sin (negative sin in the first one)
+        x_out = torch.stack(
+            [
+                x_shaped[..., 0] * rope_cache[..., 0]
+                - x_shaped[..., 1] * rope_cache[..., 1],
+                x_shaped[..., 1] * rope_cache[..., 0]
+                + x_shaped[..., 0] * rope_cache[..., 1],
+            ],
+            -1,
+        )
+        # x_out tensor has shape [b, s, n_h, h_d // 2, 2]
+        # re-collapse on the last dimension to reshape back into h_d
+        x_out = x_out.flatten(3)
+
+        # tensor has shape [b, s, n_h, h_d]
+        return x_out.type_as(x)
 
 
 if __name__ == "__main__":
     hidden_size = 4
     context_size = 1024
     x = torch.rand(1, context_size, hidden_size)
+    print(x.shape)
     # pe = SinusoidalPE(hidden_size, context_size)
     # print(pe(x).size())
     pe = RoPE(hidden_size, context_size)
-    print(pe(x).size())
+    print(pe(x).shape)
+    re = RotaryPositionalEmbeddings(4)
